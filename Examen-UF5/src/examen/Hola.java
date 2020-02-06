0bae9ce15e473641454447af11b074485b4a179b
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-02-06 17:55:37 +0100

    hola

---
+++ Examen-UF5/src/examen/Hola.java
@@ -0,0 +1,18 @@
+package examen;
+
+/**
+ *
+ * @author Daniel Aagaard Pérez
+ *
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        aaa();
+    }
+
+    public static void aaa() {
+        String a = null;
+        a.charAt(0);
+    }
+}
